b1c0edbe8e6784557ae3574de89cc4b0188c02de
ReDxDaGer <yashmehrotra45@gmail.com>
2024-05-07 18:26:49 +0530

    Made the authlink responsive

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -12,19 +12,31 @@ const AuthLinks = () => {
 
   return (
     <>
-      {status  === "notauthenticated" ? (<Link href={'/login'}>Login</Link>) : (
+      {status  === "notauthenticated" ? (<Link href={'/login'} className={styles.link}>Login</Link>) : (
         <> 
-        <Link href={'/write'}>Write</Link>
+        <Link href={'/write'} className={styles.link}>Write</Link>
         <span className={styles.link}>Logout</span>
         </>
     )}
-    <div className={styles.burger}>
+    <div className={styles.burger} onClick={()=>setOpen(!open)}>
       <div className={styles.line}></div>
       <div className={styles.line}></div>
       <div className={styles.line}></div>
-
     </div>
-  </>
+    {open && (
+      <div className={styles.responsiveMenu}>
+        <Link href={'/'}>Home</Link>
+        <Link href={'/about'}>About</Link>
+        <Link href={'/contact'}>Contact</Link>
+        {status  === "notauthenticated" ? (<Link href={'/login'}>Login</Link>) : (
+        <> 
+          <Link href={'/write'}>Write</Link>
+          <span className={styles.link}>Logout</span>
+        </>
+        )}
+      </div>
+    )}  
+    </>
   )
 }
 
